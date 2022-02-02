d3e52adb16b76026b40081cbcbebf4dc47d07f02
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-03 00:35:57 +0200

    Final version with comments

--- Task #2.py
+++ Task #2.py
@@ -1,9 +1,10 @@
-# 1. create a list of random number of dicts (from 2 to 10)
-#
-# dict's random numbers of keys should be letter,
-# dict's values should be a number (0-100),
-# example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+"""
+1. create a list of random number of dicts (from 2 to 10)
 
+dict's random numbers of keys should be letter,
+dict's values should be a number (0-100),
+example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+"""
 
 # import section
 import random
@@ -12,53 +13,47 @@ import string
 # declaring section
 dict_list = []
 result_dict = {}
-count_of_dicts = int, 0
 common_count_of_dicts = 0
 count_of_keys_in_dict = 0
 
 # generating number of dicts
 count_of_dicts = random.randint(2, 10)
-print('count_of_dicts:', count_of_dicts)
+# print('count_of_dicts:', count_of_dicts)
+
 while common_count_of_dicts < count_of_dicts:
-
-    # generating random lenght list of unique keys for each dictionary
-    # declaring and re-creating for next circle
-    count_of_keys_in_dict = random.randint(2, 10)
-    print('count_of_keys_in_dict:', count_of_keys_in_dict)
-    keys_list = []
     dict_a = {}
-
+    # generating random length list of unique keys for each dictionary
+    count_of_keys_in_dict = random.randint(2, 26)
+    # print('count_of_keys_in_dict:', count_of_keys_in_dict)
     keys_list = random.sample(string.ascii_lowercase, count_of_keys_in_dict)
-    # print(keys_list)
-    # appending dictionaries
+    # print('keys_list:', keys_list)
+
+    # generating dictionaries
     for i in keys_list:
-        random_key = i
         random_value = random.randint(1, 100)
         # print(random_key, random_value)
-        dict_a[random_key] = random_value
-        # print(len(keys_list))
-        # print(dict_a)
+        dict_a[i] = random_value
 
-    dict_list.append(dict_a)
-    common_count_of_dicts = common_count_of_dicts + 1
+    dict_list.append(dict_a)  # appending dict_list
+    common_count_of_dicts = common_count_of_dicts + 1  # rising count by 1
 # print(key)
 # print(random_key)
 # print(random_value)
-print(dict_list)
-"""
-# 2. get previously generated list of dicts and create one common dict:
-#
-# if dicts have same key, we will take max value, and rename key with dict number with max value
-# if key is only in one dict - take it as is,
-# example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+# print(dict_list)
 
-keys = []
-keys_numbered = []
+"""
+2. get previously generated list of dicts and create one common dict:
+
+if dicts have same key, we will take max value, and rename key with dict number with max value
+if key is only in one dict - take it as is,
+example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+"""
+
 values = []
 final_dict = {}
 dict_with_indexes = {}  # key:number_of_dict
 dict_with_max_values = {}  # key:max_value
-dict_is_has_duplicates = {}  # 0 or 1 in case if there's duplicate, but first value is bigger
+dict_is_has_duplicates = {}  # 0 or 1 in case if there are duplicates, but first value is bigger
 
 for i in dict_list:
     for key, value in i.items():
@@ -96,8 +91,10 @@ for i in dict_list:
 # print('keys with indexes:', dict_with_indexes)
 # print('dict_is_has_duplicates:', dict_is_has_duplicates)
 
+# Appending new dictionary key = key with dictionary number in order : value = MAX value for this key in all    /n
+# dictionaries and MIN dictionary number in order in case of duplicates
+
 for key, value in dict_with_indexes.items():
     final_dict[value] = dict_with_max_values.get(key)
 
 print('final_dict:', final_dict)
-"""
