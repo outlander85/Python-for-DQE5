942de18d8baf668d13ce3cd65ae66eb21083d5c7
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-02 23:28:25 +0200

    + final version of task #2

--- Task #2.py
+++ Task #2.py
@@ -11,15 +11,19 @@ import string
 
 # declaring section
 dict_list = []
-keys_list = []
-dict_a = {}
 result_dict = {}
+# keys_list = []
+# dict_a = {}
 
 # generating number of dicts
 for r in range(2, random.randint(2, 10)):
 
     # generating random lenght list of unique keys for each dictionary
-    keys_list = random.sample(string.ascii_letters, random.randint(2, 10))
+    # declaring and re-creating for next circle
+    keys_list = []
+    dict_a = {}
+
+    keys_list = random.sample(string.ascii_lowercase, random.randint(2, 10))
 
     # appending dictionaries
     for i in keys_list:
@@ -27,7 +31,8 @@ for r in range(2, random.randint(2, 10)):
         random_value = random.randint(1, 100)
         # print(random_key, random_value)
         dict_a[random_key] = random_value
-
+        # print(len(keys_list))
+        # print(dict_a)
 
     dict_list.append(dict_a)
 
@@ -41,3 +46,51 @@ print(dict_list)
 # if dicts have same key, we will take max value, and rename key with dict number with max value
 # if key is only in one dict - take it as is,
 # example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+
+keys = []
+keys_numbered = []
+values = []
+final_dict = {}
+dict_with_indexes = {}  # key:number_of_dict
+dict_with_max_values = {}  # key:max_value
+dict_is_has_duplicates = {}  # 0 or 1 in case if there's duplicate, but first value is bigger
+
+for i in dict_list:
+    for key, value in i.items():
+        if key not in dict_with_max_values or (dict_with_max_values and value > dict_with_max_values.get(key)):
+            dict_with_max_values[key] = value
+
+for i in dict_list:
+    for key in i.keys():
+        if key not in dict_is_has_duplicates:
+            dict_is_has_duplicates[key] = 0
+        else:
+            dict_is_has_duplicates[key] = 1
+
+co = 1
+for i in dict_list:
+    for key, value in i.items():
+        if co == 1:
+            dict_with_indexes[key] = key
+
+        elif co != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] == 0:
+            dict_with_indexes[key] = key
+
+        elif co != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] != 0:
+            dict_with_indexes[key] = key + '_' + str(co)
+
+        elif co != 1 and key in dict_with_indexes \
+                and value == dict_with_max_values.get(key) \
+                and key == dict_with_indexes.get(key):
+            dict_with_indexes[key] = key + '_' + str(co)
+    co = co + 1
+
+# print('keys and max values:', dict_with_max_values)
+# print('keys with duplicates:', dict_is_has_duplicates)
+# print('keys with indexes:', dict_with_indexes)
+# print('dict_is_has_duplicates:', dict_is_has_duplicates)
+
+for key, value in dict_with_indexes.items():
+    final_dict[value] = dict_with_max_values.get(key)
+
+print('final_dict:', final_dict)
--- for test.py
+++ for test.py
@@ -9,9 +9,118 @@
 #     return ''.join(random.choices(condition, k=str_len))
 
 
-try:
-  print(x)
-except:
-  print("Something went wrong")
-finally:
-  print("The 'try except' is finished")
+# try:
+#   print(x)
+# except:
+#   print("Something went wrong")
+# finally:
+#   print("The 'try except' is finished")
+
+
+a = [{'j': 27, 'd': 10},
+     {'l': 81, 'j': 77, 'p': 100, 'g': 84},
+     {'s': 98, 'j': 35, 'c': 63, 'h': 66, 'z': 29, 'u': 62, 'o': 92, 't': 90},
+     {'t': 80, 'c': 63},
+     {'j': 77}]
+keys = []
+keys_numbered = []
+values = []
+final_dict = {}
+dict_with_indexes = {}  # key:number_of_dict
+dict_with_max_values = {}  # key:max_value
+dict_is_has_duplicates = {}  # 0 or 1 in case if there's duplicate, but first value is bigger
+
+for i in a:
+    for key, value in i.items():
+        if key not in dict_with_max_values or (dict_with_max_values and value > dict_with_max_values.get(key)):
+            dict_with_max_values[key] = value
+
+for i in a:
+    for key in i.keys():
+        if key not in dict_is_has_duplicates:
+            dict_is_has_duplicates[key] = 0
+        else:
+            dict_is_has_duplicates[key] = 1
+
+co = 1
+for i in a:
+    for key, value in i.items():
+        if co == 1:
+            dict_with_indexes[key] = key
+
+        elif co != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] == 0:
+            dict_with_indexes[key] = key
+
+        elif co != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] != 0:
+            dict_with_indexes[key] = key + '_' + str(co)
+
+        elif co != 1 and key in dict_with_indexes \
+                and value == dict_with_max_values.get(key) \
+                and key == dict_with_indexes.get(key):
+            dict_with_indexes[key] = key + '_' + str(co)
+    co = co + 1
+
+# print('keys and max values:', dict_with_max_values)
+# print('keys with duplicates:', dict_is_has_duplicates)
+# print('keys with indexes:', dict_with_indexes)
+# print('dict_is_has_duplicates:', dict_is_has_duplicates)
+
+for key, value in dict_with_indexes.items():
+    final_dict[value] = dict_with_max_values.get(key)
+
+print('final_dict:', final_dict)
+
+# k = 1
+# for i in a:
+#     for key, value in i.items():
+#         if k == 1:
+#             dict_with_indexes[key] = key
+#         else:
+#             dict_with_indexes[key] = key + '_' + str(k)
+#     k = k + 1
+# print(dict_with_indexes)
+
+
+#
+#
+# for i in a:
+#
+#     for key, value in i.items():
+#
+#         if k == 1:
+#             key_v = key
+#         else:
+#             if key in keys:
+#                 key_v = key + '_' + str(k)
+#             else:
+#                 key_v = key
+#         val_v = value
+#         keys.append(key_v)
+#         values.append(value)
+#
+#     k = k + 1
+# # print(keys, values)
+#
+# keys_and_values = dict(zip(keys, values))
+# print(keys_and_values)
+
+# for key, value in keys_and_values.items():
+#     if
+"""
+a1 = ['a', 'b', 'c', 'a', 'a', 'b']
+a2 = []
+
+d = {}
+
+for i in a1:
+
+    d.setdefault(i, 0)
+    d[i] += 1
+
+    if d[i] >= 2:
+        a2.append('%s_%d' % (i, d[i]))
+    else:
+        a2.append(i)
+
+print(a2)
+"""
