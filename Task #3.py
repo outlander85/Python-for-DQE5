674b3553207f7f791b5a32edde21e3db5c5f30e1
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-07 01:24:41 +0200

    + Corrections

--- Task #3.py
+++ Task #3.py
@@ -1,3 +1,6 @@
+import string
+import re
+
 variable = """homEwork:
 	tHis iz your homeWork, copy these Text to variable. 
 
@@ -7,41 +10,40 @@ variable = """homEwork:
 
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
-import string
-import re
+
 a = 0
 variable = variable.lower()
-print('variable:', repr(variable))
+# print('variable:', repr(variable))
 
 # Delete multi-\n's
 a2 = variable.split("\n")
-print('a2:', a2)
+# print('a2:', a2)
 while a < len(a2):
     if a2[a] == '' or a2[a] == ' ':
         a2.remove(a2[a])
     else:
         a = a+1
-print('Modified a2:', a2)
+# print('Modified a2:', a2)
 
 a3 = [x.strip().capitalize() for x in a2]
-print('a3:', a3)
+# print('a3:', a3)
 
 a4 = "\n\t".join(a3)
-print('a4:', a4)
-print('a4:', repr(a4))
+# print('a4:', a4)
+# print('a4:', repr(a4))
 
 a5 = a4.replace('“iz”', ' “iz”').replace(' iz ', ' is ')
-print('a5:', a5)
+# print('a5:', a5)
 
 additional_sentence_words = []
 a6 = a5.split()
-print(a6)
+# print('a6:', a6)
 
 for i in range(len(a6)):
     if re.findall('[.!?]', a6[i]):
         additional_sentence_words.append(re.sub('[.!?]', '', a6[i]))
 additional_sentence = ' '.join(additional_sentence_words).capitalize() + '.'
-print(additional_sentence)
+# print(additional_sentence)
 
 final_sentence = a5 + ' \n\t' + additional_sentence
 print(final_sentence)
@@ -51,7 +53,7 @@ for i in range(0, len(final_sentence)):
     if final_sentence[i] in string.whitespace:
         cnt += 1
 
-print('\n Number of whitespaces is:', cnt)
+print('\nNumber of whitespaces is:', cnt)
 
 # d = 0
 # while b < len(a4):
