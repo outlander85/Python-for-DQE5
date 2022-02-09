899f5aadfb9fa353aba18c9214eed50a514f5a2e
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-09 20:24:56 +0200

    4-2 final version

--- Task #4-2.py
+++ Task #4-2.py
@@ -12,35 +12,78 @@ import random
 import string
 
 # declaring section
-dict_list = []
 result_dict = {}
-common_count_of_dicts = 0
-count_of_keys_in_dict = 0
+
+
+def rand_numeric(min_element=1, max_element=100):
+    return random.randint(min_element, max_element)
+
+
+def gen_dict(cod):
+    common_count_of_dicts = 0
+    dl = []
+    while common_count_of_dicts < cod:
+        dict_a = {}
+        # generating random length list of unique keys for each dictionary
+        keys_list = random.sample(string.ascii_lowercase, rand_numeric(2, 26))
+        # generating dictionaries
+        for i in keys_list:
+            random_value = rand_numeric(1, 100)
+            # print(random_key, random_value)
+            dict_a[i] = random_value
+            # print(random_value)
+        dl.append(dict_a)  # appending dict_list
+        common_count_of_dicts += 1  # rising count by 1
+    return dl
+
+
+def dict_with_max_values_f(dl):  # Dictionary list
+    dmv = {}
+    for i in dl:
+        for key, value in i.items():
+            if key not in dmv or (dmv and value > dmv.get(key)):
+                dmv[key] = value
+    return dmv
+
+
+def dict_is_has_duplicates_f(dl):  # Dictionary list
+    dhd = {}
+    for i in dl:
+        for key in i.keys():
+            if key not in dhd:
+                dhd[key] = 0
+            else:
+                dhd[key] = 1
+    return dhd
+
 
 # generating number of dicts
-count_of_dicts = random.randint(2, 10)
-# print('count_of_dicts:', count_of_dicts)
-
-while common_count_of_dicts < count_of_dicts:
-    dict_a = {}
-    # generating random length list of unique keys for each dictionary
-    count_of_keys_in_dict = random.randint(2, 26)
-    # print('count_of_keys_in_dict:', count_of_keys_in_dict)
-    keys_list = random.sample(string.ascii_lowercase, count_of_keys_in_dict)
-    # print('keys_list:', keys_list)
-
-    # generating dictionaries
-    for i in keys_list:
-        random_value = random.randint(1, 100)
-        # print(random_key, random_value)
-        dict_a[i] = random_value
-
-    dict_list.append(dict_a)  # appending dict_list
-    common_count_of_dicts = common_count_of_dicts + 1  # rising count by 1
-# print(key)
-# print(random_key)
-# print(random_value)
+count_of_dicts = rand_numeric(2, 10)
+dict_list = gen_dict(count_of_dicts)
 # print(dict_list)
+
+
+def dict_with_indexes_f(dhd, dmv):
+    dwi = {}  # key:number_of_dict
+    cnt = 1
+    for i in dict_list:
+        for key, value in i.items():
+            if cnt == 1:
+                dwi[key] = key
+
+            elif cnt != 1 and key not in dwi and dict_is_has_duplicates[key] == 0:
+                dwi[key] = key
+
+            elif cnt != 1 and key not in dwi and dict_is_has_duplicates[key] != 0:
+                dwi[key] = key + '_' + str(cnt)
+
+            elif cnt != 1 and key in dwi \
+                    and value == dict_with_max_values.get(key) \
+                    and key == dwi.get(key):
+                dwi[key] = key + '_' + str(cnt)
+        cnt = cnt + 1
+    return dwi
+
 
 """
 2. get previously generated list of dicts and create one common dict:
@@ -52,45 +95,16 @@ example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
 
 values = []
 final_dict = {}
-dict_with_indexes = {}  # key:number_of_dict
-dict_with_max_values = {}  # key:max_value
-dict_is_has_duplicates = {}  # 0 or 1 in case if there are duplicates, but first value is bigger
 
-for i in dict_list:
-    for key, value in i.items():
-        if key not in dict_with_max_values or (dict_with_max_values and value > dict_with_max_values.get(key)):
-            dict_with_max_values[key] = value
-
-for i in dict_list:
-    for key in i.keys():
-        if key not in dict_is_has_duplicates:
-            dict_is_has_duplicates[key] = 0
-        else:
-            dict_is_has_duplicates[key] = 1
-
-cnt = 1
-for i in dict_list:
-    for key, value in i.items():
-        if cnt == 1:
-            dict_with_indexes[key] = key
-
-        elif cnt != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] == 0:
-            dict_with_indexes[key] = key
-
-        elif cnt != 1 and key not in dict_with_indexes and dict_is_has_duplicates[key] != 0:
-            dict_with_indexes[key] = key + '_' + str(cnt)
-
-        elif cnt != 1 and key in dict_with_indexes \
-                and value == dict_with_max_values.get(key) \
-                and key == dict_with_indexes.get(key):
-            dict_with_indexes[key] = key + '_' + str(cnt)
-    cnt = cnt + 1
+dict_with_max_values = dict_with_max_values_f(dict_list)  # key:max_value
+dict_is_has_duplicates = dict_is_has_duplicates_f(dict_list)  # 0 or 1 in case if there are duplicates, first is bigger
+dict_with_indexes = dict_with_indexes_f(dict_is_has_duplicates, dict_with_max_values)
 
 # Print version used for debug
+# print('dict_list:', dict_list)
 # print('keys and max values:', dict_with_max_values)
 # print('keys with duplicates:', dict_is_has_duplicates)
 # print('keys with indexes:', dict_with_indexes)
-# print('dict_is_has_duplicates:', dict_is_has_duplicates)
 
 # Appending new dictionary key = key with dictionary number in order : value = MAX value for this key in all    /n
 # dictionaries and MIN dictionary number in order in case of duplicates
