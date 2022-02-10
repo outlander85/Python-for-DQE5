0a169c557e9b60a39d86f0396feb0627c7538374
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-10 22:51:58 +0200

    + Task #4-3

--- Task #4-3.py
+++ Task #4-3.py
@@ -12,19 +12,73 @@ variable = """homEwork:
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
 
-a = 0
-variable = variable.lower()
+
+def lower_f(var):
+    var = var.lower()
+    return var
+
+
+def split_f(str_row='', delimeter="\n"):
+    str_row = str_row.split(delimeter)
+    return str_row
+
+
+def delete_ns_f(str_row='', delimeter="\n"):
+    a = 0
+    a2 = split_f(str_row, delimeter)
+    while a < len(a2):
+        if a2[a] == '' or a2[a] == ' ':
+            a2.remove(a2[a])
+        else:
+            a = a + 1
+    return a2
+
+
+def additional_sentence_f(str_row=''):
+    additional_sentence_words = []
+    a6 = str_row.split()
+
+    for i in range(len(a6)):
+        if re.findall('[.!?]', a6[i]):
+            additional_sentence_words.append(re.sub('[.!?]', '', a6[i]))
+    additional_sentence = ' '.join(additional_sentence_words).capitalize()
+    return additional_sentence
+
+
+def final_sentence_f(str_row, add_sentence):
+    final_sentence = []
+    a7 = str_row.split('.')
+    while a7:
+        a8 = a7.pop(0)
+        counter = 0
+        for i in a8:
+            if not i.isalpha():
+                counter += 1
+            elif i == ' ':
+                replace(a8, '')
+            else:
+                break
+        a8 = a8[:counter] + a8[counter:].capitalize()
+        if 'this paragraph' in a8:
+            a8 = a8 + '. ' + add_sentence
+        final_sentence.append(a8)
+    final_sentence = '.'.join(final_sentence)
+    return final_sentence
+
+
+def count_of_whitespaces(str_row):
+    cnt = 0
+    for i in range(0, len(str_row)):
+        if str_row[i] in string.whitespace:
+            cnt += 1
+    return cnt
+
+
+variable = lower_f(variable)
 # print('variable:', repr(variable))
 
-# Delete multi-\n's
-a2 = variable.split("\n")
+a2 = delete_ns_f(variable, "\n")
 # print('a2:', a2)
-while a < len(a2):
-    if a2[a] == '' or a2[a] == ' ':
-        a2.remove(a2[a])
-    else:
-        a = a+1
-# print('Modified a2:', a2)
 
 a3 = [x.strip() for x in a2]
 # print('a3:', a3)
@@ -36,42 +90,12 @@ a4 = "\n\t".join(a3)
 a5 = a4.replace('“iz”', ' “iz”').replace(' iz ', ' is ')
 # print('a5:', a5)
 
-additional_sentence_words = []
-a6 = a5.split()
-# print('a6:', a6)
-
-for i in range(len(a6)):
-    if re.findall('[.!?]', a6[i]):
-        additional_sentence_words.append(re.sub('[.!?]', '', a6[i]))
-additional_sentence = ' '.join(additional_sentence_words).capitalize()
+additional_sentence = additional_sentence_f(a5)
 # print(additional_sentence)
 
-final_sentence = []
-# last_words = 'Here should be the last sentence'
+final_sentence = final_sentence_f(a5, additional_sentence)
 
-a7 = a5.split('.')
-# print(a7)
-while a7:
-    a8 = a7.pop(0)
-    counter = 0
-    for i in a8:
-        if not i.isalpha():
-            counter += 1
-        elif i == ' ':
-            replace(a8, '')
-        else:
-            break
-    a8 = a8[:counter] + a8[counter:].capitalize()
-    if 'this paragraph' in a8:
-        a8 = a8 + '. ' + additional_sentence
-    final_sentence.append(a8)
-final_sentence = '.'.join(final_sentence)
+cnt = count_of_whitespaces(final_sentence)
+
 print(final_sentence)
-
-cnt = 0
-for i in range(0, len(final_sentence)):
-    if final_sentence[i] in string.whitespace:
-        cnt += 1
-
 print('\nNumber of whitespaces is:', cnt)
-
