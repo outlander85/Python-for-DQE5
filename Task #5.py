6d80f65cd2c762f3d49f424a533a5eccb658acde
Ihor_Ivchenko <ivivchenko@gmail.com>
2022-02-22 00:11:04 +0200

    Final variant of task #5

--- Task #5.py
+++ Task #5.py
@@ -23,74 +23,130 @@ You need to implement:
 
 Each new record should be added to the end of file. Commit file in git for review.
 """
+from datetime import datetime, timedelta
+import os
 
 
 class Publication:
-
     def __init__(self):
-        pass
+        self.header = ''
+        self.article_type = ''
+        self.date_type = ''
+        self.article_body = ''
+        self.date = ''
+        self.valid_days = ''
+        self.addinfo = ''
 
-    def variant(self, variant):
-        pass
+    def get_publication_body(self, text='input'):
+        if text == 'input':
+            print(f'Input {self.article_type} body\n')
+            text = input()
+        self.article_body = text
 
+    def get_city_and_cur_date(self, text='input'):
+        if text == 'input':
+            print(f'Input city\n')
+            text = input()
+        self.addinfo = text + ', ' + str(datetime.now())
 
-class Input:
+    def get_date(self, text='input'):
+        if text == 'input':
+            print(f'Input {self.date_type} in dd/mm/yyyy format\n')
+            text = input()
+        self.date = text
 
-    def __init__(self):
-        pass
+    def get_valid_days(self, text='input'):
+        if text == 'input':
+            print(f'Input {self.date_type} - count of days (use digits)\n')
+            text = input()
+        self.valid_days = text
 
-    def choose_input_mode(self):
-        choose = ''
-        print('Please choose publication variant:\n1 - for %s\n2 - for %s\n3 - for %s' %
-              ('NEWS', 'ADVERTISEMENT', 'PROMOCODE'))
-        try:
-            choose = int(input())
-        except ValueError:
-            print("You've entered wrong value")
-
-        if choose == 1:
-            mode = 'NEWS'
-        elif choose == 2:
-            mode = 'ADVERTISEMENT'
-        elif choose == 3:
-            mode = 'PROMOCODE'
-        else:
-            # print('Please make correct choice')
-            mode = None
-
-        if choose in (1, 2, 3):
-            print(f"You've chosen {mode}")
-        else:
-            print('Please make correct choice')
-
-        return choose
-
-    def input_news(self):
-        pass
-
-    def input_adv(self):
-        pass
-
-
-class Calc:
-    pass
+    def write(self):
+        f = open(os.path.abspath('LOG.txt'), 'a')
+        f.write('\n'.join([self.header, self.article_body, self.addinfo, '\n----\n\n']))
+        f.close()
 
 
 class AddNews(Publication):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.header = 'News:'
+        self.article_type = 'News'
 
 
-class AddAdvertisement:
-    pass
+class AddAdvertisement(Publication):
+
+    def __init__(self):
+        super().__init__()
+        self.header = 'Advertisement:'
+        self.article_type = 'Adv'
+        self.date_type = 'Exp.date'
+
+    def adv_calc(self):
+        self.get_date()
+        days = datetime.date(datetime.strptime(self.date, '%d/%m/%Y')) - datetime.date(datetime.now())
+        self.addinfo = f'Actual until: {self.date}, {days.days} days left'
 
 
-class AddPromoCode:
-    pass
+class AddPromoCode(Publication):
+
+    def __init__(self):
+        super().__init__()
+        self.header = 'New PromoCode'
+        self.type = 'PromoCode'
+        self.date_type = 'Valid days'
+
+    def promo_code_calc(self):
+        self.get_valid_days()
+        final_date = datetime.date(datetime.now()) + timedelta(days=int(self.valid_days))
+        self.addinfo = f'Proposition valid {self.valid_days} days, actual before: {str(final_date)}'
 
 
-class WriteLog:
-    pass
+class Main:
+    def __init__(self):
+        self.main_message = 'Please choose publication variant:\n1 - for %s\n2 - for %s\n3 - for %s\n4 - to %s' % \
+                            ('NEWS', 'ADVERTISEMENT', 'PROMOCODE', 'FINISH PROGRAM')
+        self.error_message = 'Please make correct choice'
+        self.date_error_message = 'Program will be aborted - please enter correct date next time\n'
+        pass
+
+    def text_feed_add(self):
+        print(self.main_message)
+        choice = input()
+        if choice == '1':
+            news = AddNews()
+            news.get_publication_body()
+            news.get_city_and_cur_date()
+            news.write()
+            self.text_feed_add()
+        elif choice == '2':
+            adv = AddAdvertisement()
+            adv.get_publication_body()
+            try:
+                adv.adv_calc()
+                adv.write()
+            except ValueError:
+                print(self.date_error_message)
+            finally:
+                self.text_feed_add()
+        elif choice == '3':
+            promocode = AddPromoCode()
+            promocode.get_publication_body()
+            try:
+                promocode.promo_code_calc()
+                promocode.write()
+            except ValueError:
+                print(self.date_error_message)
+            finally:
+                self.text_feed_add()
+        elif choice == '4':
+            return
+        else:
+            print(self.error_message)
+            self.text_feed_add()
 
 
-Input.choose_input_mode(self=None)
-print(Input.choose_input_mode.choose)
+if __name__ == "__main__":
+    Main().text_feed_add()
+
